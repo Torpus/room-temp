77ee26cf82e06cafebaff3a4cf9a7230e0504774
Torpus <torpus@hotmail.com>
2020-07-06 23:50:25 -0500

    fix: wasnt bringing in PROM_IP_ADDR

--- resources/data.js
+++ resources/data.js
@@ -1,6 +1,7 @@
 const HUE_IP_ADDR = process.env.HUE_IP_ADDR;
 const HUE_API_KEY = process.env.HUE_API_KEY;
 const MQTT_IP_ADDR = process.env.MQTT_IP_ADDR;
+const PROM_IP_ADDR = process.env.PROM_IP_ADDR;
 
 module.exports = {
   hueUrl: `http://${HUE_IP_ADDR}/api/${HUE_API_KEY}/sensors`,
